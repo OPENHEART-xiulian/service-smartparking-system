73f069c5edea79741774d4a74f3831c900e4bf5d
zhaowenyi <zhaowenyi@etongeis.com>
2024-10-10 14:49:35 +0800

    初始化

--- smartparking/src/test/java/com/sp/cjgc/SmartparkingApplicationTests.java
+++ smartparking/src/test/java/com/sp/cjgc/SmartparkingApplicationTests.java
@@ -1,6 +1,5 @@
 package com.sp.cjgc;
 
-import com.sp.cjgc.backend.vehiclebilling.CaptureDataUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,7 +13,5 @@ class SmartparkingApplicationTests {
         String cameraIp = "192.88.233.19";
         short wLinkPort = 8000;
         String sLicense = "粤A11111";
-
-        CaptureDataUtil.createCaptureData(sLicense, cameraIp, wLinkPort);
     }
 }
